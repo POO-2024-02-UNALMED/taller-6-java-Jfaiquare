e3690d72ac721abb8ba827be3c5f7420a297097e
Jahnear Isaiah Faiquare Vizcaíno <jfaiquare@unal.edu.co>
2024-12-17 13:55:35 -0500

    Delete src/main/java/Automóvil.java

--- src/main/java/Automóvil.java
+++
@@ -1,9 +0,0 @@
-public class Automovil extends Vehiculo {
-    private int puertas;
-
-    public Automovil(String placa, String nombre, int precio, int peso, Fabricante fabricante, int puertas) {
-        super(placa, puertas, 100, nombre, precio, peso, "FWD", fabricante);
-        this.puertas = puertas;
-    }
-
-}
